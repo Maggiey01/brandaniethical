bb96c8a0556809e5f3070f868062dc1a472e99ab
Maggiey01 <90772815+Maggiey01@users.noreply.github.com>
2021-12-16 12:51:04 -0500

    Added working version of test

--- src/brandaniethical/brandaniethical.py
+++ src/brandaniethical/brandaniethical.py
@@ -1,11 +1,13 @@
 import re
-import lxml
-from lxml import html
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+from requests.exceptions import HTTPError
+import requests.auth
+import os
+# function 1: get fashion brands' animal ethical info 
 def brandaniethical(brandlist):
+    
     """
     Get fashion brands' animal ethical usage information. provide information of whether the brand use fur, angora, down feather, shearling, karakul, exotic animal skin and hair, wool use including ‘mulesing’, and leather 
     Also check whether brands use these material align with standards.
@@ -23,18 +25,21 @@ def brandaniethical(brandlist):
     Examples
     --------
     >>> from brandaniethical import brandaniethical 
-    >>> fashion_animaterial(['cos','theory','lululemon','nike', 'skechers'])
+    >>> brandaniethical.brandaniethical(['cos','theory','lululemon','nike', 'skechers'])
 	company	animal_score	update	fur	angora	leather	wool	shearling	karakul	down	feather	exotic animal hair	exotic animal skin	down_RDS	wool_mulesing
-0	cos	3 out of 5	Last Updated: July 2020	0	0	1	1	0	0	1	0	1	0	1	1
-1	theory	2 out of 5	Last Updated: December 2019	0	0	1	1	0	0	1	0	1	0	0	0
-2	lululemon	2 out of 5	Last Updated: July 2020	0	0	1	1	0	0	1	0	1	0	1	0
-3	nike	2 out of 5	Last Updated: July 2020	0	0	1	1	0	0	1	0	1	0	0	0
-4	skechers	2 out of 5	Last Updated: August 2020	0	0	1	1	0	0	0	0	0	0	0	0
+0	cos	3/5	July 2020	0	0	1	1	0	0	1	0	1	0	1	1
+1	theory	2/5	December 2019	0	0	1	1	0	0	1	0	1	0	0	0
+2	lululemon	2/5	July 2020	0	0	1	1	0	0	1	0	1	0	1	0
+3	nike	2/5	July 2020	0	0	1	1	0	0	1	0	1	0	0	0
+4	skechers	2/5	August 2020	0	0	1	1	0	0	0	0	0	0	0	0
     """
+    assert isinstance(brandlist, list), f"This function only works on list but brandlist={brandlist}."
+    for b in brandlist:
+        assert isinstance(b, str), f"This function only works on string but brand={b}."
     brandlist = [b.lower() for b in brandlist]
-    ani_reg = '[^.]* animal [^.]*\.'
-    use_reg = '[^.]* (uses) [^.]*\.'
-    date_reg = '(Last Updated: )(\w*)'
+    ani_reg = r'[^.]* animal [^.]*\.'
+    use_reg = r'[^.]* (uses) [^.]*\.'
+    date_reg = r'(Last Updated: )(\w*\s\d...)'
     web = 'https://directory.goodonyou.eco/'
     animal_score = []
     ani_rate= []
@@ -61,7 +66,8 @@ def brandaniethical(brandlist):
         return df_gfy
     global dfydf
     dfydf = brandani(brandlist)
-            #check animal material usage
+    #check animal material usage
+    #data wrangling
     try:
         
         dfydf['ani_material'] = [re.search(use_reg, i).group(0) for i in dfydf['ani_rate']]
@@ -71,14 +77,16 @@ def brandaniethical(brandlist):
         
 
     else:
+        dfydf['update'] = [re.search(r'\w*\s\d...', i).group(0) for i in dfydf['update']]
+        dfydf['animal_score'] = [re.sub(r' out of ', '/', i) for i in dfydf['animal_score']]
         fabric = ['fur','angora', 'leather', 'wool', 'shearling', 'karakul', 'down', 'feather', 'exotic animal hair', 'exotic animal skin']
         for f in fabric:
             dfydf[f]=dfydf['ani_material'].str.contains(f)
-                #check if use down whether down accredited by the Responsible Down Standar
+        #check if use down whether down accredited by the Responsible Down Standar
         RDS_reg = 'It uses down accredited by the Responsible Down Standar'
         dfydf['down_RDS'] = dfydf['ani_rate'].str.contains(RDS_reg)
         dfydf.loc[dfydf['down_RDS']==True,'down'] = True
-                #check if use wool whether wool includes mulesing
+        #check if use wool whether wool includes mulesing
         dfydf['wool_mulesing'] = dfydf['ani_rate'].str.contains('mules')
         dfydf.loc[dfydf['wool_mulesing']==True,'wool'] = True
         df_aniusage = dfydf.loc[:,dfydf.columns!='ani_material']
@@ -87,3 +95,136 @@ def brandaniethical(brandlist):
         
         return df_material.loc[:,df_material.columns!='ani_rate']
        
+       
+    
+# function 2: get region trendytickers esg score       
+import requests
+from requests.exceptions import HTTPError
+import requests.auth
+import re
+import pandas as pd
+import os
+regionlist = ['US', 'BR', 'AU', 'CA', 'FR', 'DE', 'HK', 'IN', 'IT', 'ES', 'GB', 'SG']
+YAHOOF_KEY = os.getenv('YAHOOF_KEY')
+app_key = os.getenv('ESG_KEY')
+
+def trendyesg(region, start=5):
+    """
+    Get region's trendy tickers' esg score based on given region and regular rank of market price.
+
+    Parameters
+    ----------
+    region : string
+      A string that limit the region of market consumers are interested in, which include US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, and SG.
+    start : int
+      A int that indicate the ranking .
+
+    Returns
+    -------
+    pandas.dataframe
+      The dataframe of trendy tickers' esg, environmental, social,governance score, and its ticker based on consumers interested region and the lowest ranking consumers want to start to look at
+
+    Examples
+    --------
+    >>> from brandaniethical import trendyesg 
+    >>> trendyesg(region='US',start =8)
+	company_name	env_score	soc_score	gov_score	total	ticker
+0	Medtronic plc	500	304	310	1114	MDT
+1	Pfizer Inc.	510	300	305	1115	PFE
+2	Roblox Corporation	245	304	220	769	RBLX
+3	Lemonade, Inc.	520	303	300	1123	LMND
+4	Nucor Corporation	251	326	210	787	NUE
+    """
+    assert region in regionlist, f"This function only works on {regionlist} but region={region}."
+    assert isinstance(start, int), f"This function only works on int but start={start}."
+    def get_trendycom(region, start=5):
+        url = "https://yh-finance.p.rapidapi.com/market/get-trending-tickers"
+        def trendytick(region):
+            querystring = {"region":region}
+            try:
+                headers = {
+                    'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+                    'x-rapidapi-key': YAHOOF_KEY
+                            }
+                response = requests.request("GET", url, headers=headers, params=querystring)
+                response.raise_for_status()
+
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err: 
+                print(f'Other error occurred: {err}')
+            return response.json()
+
+        cominfolist = trendytick(region)
+
+        region = []
+        shortName = []
+        regularMarketPrice = []
+        symbol=[]
+        trendtick_df = pd.DataFrame()
+    
+        for i in cominfolist['finance']['result'][0]['quotes']:   
+            shortName.append(i['shortName'])
+            regularMarketPrice.append(i['regularMarketPrice'])
+            symbol.append(i['symbol'])
+
+        trendtick_df['shortName'] = pd.Series(shortName)
+        trendtick_df['regularMarketPrice'] = pd.Series(regularMarketPrice)
+        trendtick_df['symbol'] = pd.Series(symbol)
+        trendtick_df['maxprice_rank'] = trendtick_df['regularMarketPrice'].rank(method='max')
+
+        return trendtick_df.sort_values(by=['maxprice_rank']).iloc[start:start+5]
+
+    #get trendy tickers outcome
+    tdf = get_trendycom(region, start=5)
+    #make general search of ticker e.g.
+    ttlist = [i for i in tdf['symbol']]
+    reg_tick = r'(^.+?)-|(^.*)'
+    for tick in ttlist:
+        ticklist=[]
+        try:
+            ticklist = [re.match(reg_tick,tick).group(1) for tick in ttlist]
+            ticklist.extend([re.match(reg_tick,tick).group(2) for tick in ttlist])
+            ticklist = set(ticklist)
+        except Exception as e:
+            print(f'{e}: the ticker is not supported')
+    ticklist = list(filter(None, ticklist))
+
+    base_url = 'https://tf689y3hbj.execute-api.us-east-1.amazonaws.com/prod/authorization/'
+    df = pd.DataFrame()
+    company_name=[]
+    env_score=[]
+    soc_score=[]
+    gov_score=[]
+    total=[]
+    region=[]
+    ticker=[]
+    for brand_tick in ticklist:
+        try:
+            response_esg = requests.get(base_url+'search?',params={'q':brand_tick,'token':app_key})
+            response_esg.raise_for_status()
+            company_name.append(response_esg.json()[0]['company_name'])
+            env_score.append(response_esg.json()[0]['environment_score'])
+            soc_score.append(response_esg.json()[0]['social_score'])
+            gov_score.append(response_esg.json()[0]['governance_score'])
+            total.append(response_esg.json()[0]['total'])
+            ticker.append(brand_tick)
+        except IndexError as ind_err:
+            company_name.append(brand_tick)
+            env_score.append('NA')
+            soc_score.append('NA')
+            gov_score.append('NA')
+            total.append('NA')
+            ticker.append(brand_tick)
+            print(f"the company's esg not in database")
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err: 
+            print(f'Other error occurred: {err}')
+    df['company_name'] = pd.Series(company_name)
+    df['env_score'] = pd.Series(env_score)
+    df['soc_score'] = pd.Series(soc_score)
+    df['gov_score'] = pd.Series(gov_score)
+    df['total'] = pd.Series(total)
+    df['ticker'] = pd.Series(ticker)
+    return df
--- tests/test_brandaniethical.py
+++ tests/test_brandaniethical.py
@@ -1,36 +1,19 @@
-from brandaniethical import brandaniethical
 import re
-import lxml
-from lxml import html
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import pytest
+from requests.exceptions import HTTPError
+import requests.auth
+import os
+from brandaniethical.brandaniethical import brandaniethical
+from brandaniethical.brandaniethical import trendyesg
+from pandas._testing import assert_frame_equal
 
+#function1 test
 df1 = pd.DataFrame(
-{'company':'nike',
- 'animal_score':'2 out of 5',
- 'update':'Last Updated: July 2020',
- 'fur':0,
- 'angora':0,
- 'leather':1,
- 'wool':1,
- 'shearling':0,
- 'karakul':0,
- 'down':1,
- 'feather':0,
- 'exotic animal hair':1,
- 'exotic animal skin':0,
- 'down_RDS':0,
- 'wool_mulesing':0
-},index=[0])
-
-ans = 'please check company name of a\nno animal material information of ani'
-
-df2 = pd.DataFrame(
 {'company':'adidas',
- 'animal_score':'3 out of 5',
- 'update':'Last Updated: July 2020',
+ 'animal_score':'3/5',
+ 'update':'July 2020',
  'fur':0,
  'angora':0,
  'leather':1,
@@ -45,22 +28,53 @@ df2 = pd.DataFrame(
  'wool_mulesing':1
 },index=[0])
 
-
+df2 = pd.DataFrame(
+{'company':'nike',
+ 'animal_score':'2/5',
+ 'update':'July 2020',
+ 'fur':0,
+ 'angora':0,
+ 'leather':1,
+ 'wool':1,
+ 'shearling':0,
+ 'karakul':0,
+ 'down':1,
+ 'feather':0,
+ 'exotic animal hair':1,
+ 'exotic animal skin':0,
+ 'down_RDS':0,
+ 'wool_mulesing':0
+},index=[0])
 
 def test_fashion_animaterial():
     example = ['adidas']
-    expected = df2
+    expected = df1
     result = brandaniethical(example)
-    assert result.equals(expected)
+    assert_frame_equal(df1, result)
+    
 
 def test_uppercase_brand():
     example2 = ['NIKE']
-    expected = df1
+    expected = df2
     result = brandaniethical(example2)
-    assert result.equals(expected)
+    assert_frame_equal(df2, result)
 
 def test_invalid_brand():
     example3 = ['a']
-    expected = ans1
     result = brandaniethical(example3)
-    assert result.equals(expected)
+    assert isinstance(result, type(None))
+    
+    
+    
+#function2 test
+import pytest
+def test_invalid_region():
+    region = 'ak'
+    with pytest.raises(AssertionError):
+        trendyesg(region, start=5)
+
+
+def test_invalid_start():
+    start = 'ak'
+    with pytest.raises(AssertionError):
+        trendyesg(region='US', start=start)
